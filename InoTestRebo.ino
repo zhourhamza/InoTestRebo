b78aa591f9689337718aef0fb15a2f7a0a064706
zhourhamza <hse1997777@yahoo.com>
2022-02-02 08:13:07 +0200

    Add comments to Ino File.

--- InoTestRebo.ino
+++ InoTestRebo.ino
@@ -1,78 +1,95 @@
+/**
+ * @author Hamza Mohammed Zhour
+ * @brief this is an arduino test project
+ * 
+ */
+#define sunLightSensor 7    //define board pin number as a variable.
+#define mother_BMR 8        //define board pin number as a variable.
+#define motherEyeMuscles 9  //define board pin number as a variable.
+#define babyEyeMuscles 10   //define board pin number as a variable.
+#define babyEyelidMuscle 11 //define board pin number as a variable.
 
-#define sunLightSensor 7                                   //define board pin number as a varible.
-#define mother_BMR 8                                       //define board pin number as a varible.
-#define motherEyeMuscles 9                                 //define board pin number as a varible.
-#define babyEyeMuscles 10                                  //define board pin number as a varible.
-#define babyEyelidMuscle 11                                //define board pin number as a varible.
+#define babyBedMotor 12 //define board pin number as a variable.
 
-#define babyBedMotor 12                                    //define board pin number as a varible.
+void setup()
+{
 
-void setup() {
+   Serial.begin(9600);
 
-  Serial.begin(9600);
-  
-  pinMode(sunLightSensor, INPUT_PULLUP);                   //Assign board pin port as an input 5v+ voltage
-  pinMode(mother_BMR, INPUT_PULLUP);
-  pinMode(motherEyeMuscles, INPUT_PULLUP);
-  pinMode(babyEyeMuscles, INPUT_PULLUP);
-  pinMode(babyEyelidMuscle, INPUT_PULLUP);
+   pinMode(sunLightSensor, INPUT_PULLUP);   //Assign board pin port as a digital 5v+ voltage input.
+   pinMode(mother_BMR, INPUT_PULLUP);       //Assign board pin port as a digital 5v+ voltage input.
+   pinMode(motherEyeMuscles, INPUT_PULLUP); //Assign board pin port as a digital 5v+ voltage input.
+   pinMode(babyEyeMuscles, INPUT_PULLUP);   //Assign board pin port as a digital 5v+ voltage input.
+   pinMode(babyEyelidMuscle, INPUT_PULLUP); //Assign board pin port as a digital 5v+ voltage input.
 
-  pinMode(babyBedMotor, OUTPUT);
-
+   pinMode(babyBedMotor, OUTPUT); //Assign board pin port as a digital 5v+ voltage output.
 }
-void loop() {
+void loop()
+{
 
-  int day_status = digitalRead(sunLightSensor);
-  int MOTHER_BMR = digitalRead(mother_BMR);
-  int MotherEyeMuscles = digitalRead(motherEyeMuscles);
-  int BabyEyeMuscles = digitalRead(babyEyeMuscles);
-  int Baby_Eyelid = digitalRead(babyEyelidMuscle);
+   int day_status = digitalRead(sunLightSensor);         // Read Day-Time which 1 == MORNING and 0 == NIGHT.
+   int MOTHER_BMR = digitalRead(mother_BMR);             // Read Mother Basal-Metabolic-Rate which 0 == LOW BMR and 1 == NORMAL/HIGH.
+   int MotherEyeMuscles = digitalRead(motherEyeMuscles); // Read Mother-Eye-Muscles which 0 == Stopped at certain position and 1 == In action.
+   int BabyEyeMuscles = digitalRead(babyEyeMuscles);     // Read Baby-Eye-Muscles which 0 == Stopped at certain position and 1 == In action.
+   int Baby_Eyelid = digitalRead(babyEyelidMuscle);      // Detect Baby-Eyelid-Closure which 0 == CLOSED and 1 == OPENED.
 
-  if (day_status == 0 && MOTHER_BMR == 0) {
+   if (day_status == 0 && MOTHER_BMR == 0) //Check if mother want to sleep and the day-time is night.
+   {
 
-    Serial.print("Night-Time, Mother want to sleep ");
+      Serial.print("Night-Time, Mother want to sleep ");
 
-    if (MotherEyeMuscles == 0 && BabyEyeMuscles == 0) {
+      if (MotherEyeMuscles == 0 && BabyEyeMuscles == 0) //Check if mother & baby eyes are in contact or not.
+      {
          Serial.print(" uh, eye contact triggered! ");
-         shaking_baby(true);
-         delay(900000);
-         shaking_baby(false);
+         shaking_baby(true);  //Run Shaking baby bed function.
+         delay(900000);       //Wait 15 min until baby sleep, 15 min needed due to eye contact triggered.
+         shaking_baby(false); //Stop Shaking baby bed function.
 
-      if (Baby_Eyelid == 0) {
-         shaking_baby(false);
-         Serial.print("Yes, Baby slept so mother will going to sleep 😴 ");
-
-      }else{
-         shaking_baby(true);
-         Serial.print("uh, baby didn't fall to sleep");
+         if (Baby_Eyelid == 0) //Check if baby-Eyelids are closed which 0 == closed.
+         {
+            shaking_baby(false); //Stop Shaking baby bed function.
+            Serial.print("Yes, Baby slept so mother will going to sleep 😴 ");
+         }
+         else
+         {
+            shaking_baby(true); //Run Shaking baby bed function.
+            Serial.print("uh, baby didn't fall to sleep");
+         }
       }
-
-    }else{
+      else
+      {
          Serial.print(" WAW, eye contact NOT triggered! ");
-         shaking_baby(true);
-         delay(180000);
-         shaking_baby(false);
+         shaking_baby(true);  //Run Shaking baby bed function.
+         delay(180000);       //Wait 3 min until baby sleep, 3 min needed due to eye contact not triggered.
+         shaking_baby(false); //Stop Shaking baby bed function.
 
-      if (Baby_Eyelid == 0) {
-         shaking_baby(false);
-         Serial.print("Yes, Baby slept so mother will going to sleep 😴 ");
-
-      }else{
-         shaking_baby(true);
-         Serial.print("uh, baby didn't fall to sleep");
+         if (Baby_Eyelid == 0) //Check if baby-Eyelids are closed which 0 == closed.
+         {
+            shaking_baby(false); //Stop Shaking baby bed function.
+            Serial.print("Yes, Baby slept so mother will going to sleep 😴 ");
+         }
+         else
+         {
+            shaking_baby(true); //Run Shaking baby bed function.
+            Serial.print("uh, baby didn't fall to sleep");
+         }
       }
-    }
-
-  }else{
-     shaking_baby(false);
-     Serial.print("Morning-Time, Mother don't want to sleep! ");
-  }
+   }
+   else
+   {
+      shaking_baby(false); //Stop Shaking baby bed function.... Defult value.
+      Serial.print("Morning-Time, Mother don't want to sleep! ");
+   }
 }
 
-void shaking_baby( boolean sleep ) {
-  if (sleep == true) {
-     digitalWrite( babyBedMotor, HIGH);
-  }else{
-     digitalWrite( babyBedMotor, LOW);
-  }
+void shaking_baby(boolean sleep)
+{
+   if (sleep == true)
+   {
+      digitalWrite(babyBedMotor, HIGH); //Run motor.
+   }
+   else
+   {
+      digitalWrite(babyBedMotor, LOW); //Stop motor.
+   }
 }
